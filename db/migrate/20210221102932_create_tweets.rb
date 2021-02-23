84ab7760f1a3dcd53cfe478282267fecf7634341
tydshun <69792117+tydshun@users.noreply.github.com>
2021-02-24 01:54:29 +0900

    Readmeの変更

---
+++ db/migrate/20210221102932_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+
+      t.string :title, null: false
+      t.text :text,    null:false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
