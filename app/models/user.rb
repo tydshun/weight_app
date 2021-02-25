23f31b97706ce5400a51a333902a4d99647220e2
tydshun <69792117+tydshun@users.noreply.github.com>
2021-02-25 17:02:48 +0900

    nicknameを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :profile, length: { maximum: 200 }
 
   devise :database_authenticatable, :registerable,
