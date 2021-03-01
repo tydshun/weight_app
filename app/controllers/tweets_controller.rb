b11125fd25c89ea76d05e4eed1d8dde7a8aeeed4
tydshun <69792117+tydshun@users.noreply.github.com>
2021-03-01 18:27:12 +0900

    新規登録のビューを作成

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,58 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit]
+  
+  def index
+    @tweets = Tweet.all
+    @tweet = Tweet.new
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to action: :index
+    else
+      render :index
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def edit
+    unless user_signed_in? && current_user.id == @tweet.user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+
+  end
+end
+
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
 class Tweet < ApplicationRecord
+<<<<<<< Updated upstream
+=======
+  # has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :text, presence: true
+  # validates :image
+>>>>>>> Stashed changes
 end
