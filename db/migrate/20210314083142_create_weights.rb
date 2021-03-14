5167fc3540cd245265040e5a61f5a380cc1ff8d9
tydshun <69792117+tydshun@users.noreply.github.com>
2021-03-14 17:47:24 +0900

    バリデーションの記述

---
+++ db/migrate/20210314083142_create_weights.rb
@@ -0,0 +1,10 @@
+class CreateWeights < ActiveRecord::Migration[6.0]
+  def change
+    create_table :weights do |t|
+
+      t.decimal :weight_body, null: false, precision: 6, scale: 2
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_064551) do
+ActiveRecord::Schema.define(version: 2021_03_14_083142) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,12 @@ ActiveRecord::Schema.define(version: 2021_02_27_064551) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "weights", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.decimal "weight_body", precision: 6, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
